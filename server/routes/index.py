98f4039276e1e48b9e881063e60c33af2402fd44
Amlan <amlannandy5@gmail.com>
2021-07-08 17:16:05 +0530

    updated api routes

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,13 +1,39 @@
 from flask import Blueprint, jsonify
 from server.models.Crop import Crop
+from server.models.Record import Record
 
-index = Blueprint('routes', __name__)
+index = Blueprint('', __name__)
 
 @index.route('/')
 def get_crop_prices():
-    crops = Crop.query.all()
-    response = {
-        'success': True,
-        'data': list(map(lambda c: c.to_json(), crops)),
-    }
-    return jsonify(response), 200
+    try:
+        crops = Crop.query.all()
+        response = {
+            'success': True,
+            'data': list(map(lambda c: c.to_json(), crops)),
+            'msg': 'Crops fetched successfully!',
+        }
+        return jsonify(response), 200
+    except:
+        response = {
+            'success': False,
+            'msg': 'Something went wrong!',
+        }
+        return jsonify(response), 500
+
+@index.route('/records')
+def get_all_records():
+    try:
+        records = Record.query.all()
+        response = {
+            'success': True,
+            'data': list(map(lambda r: r.to_json(), records)),
+            'msg': 'Records fetched successfully!',
+        }
+        return jsonify(response), 200
+    except:
+        response = {
+            'success': False,
+            'msg': 'Something went wrong!',
+        }
+        return jsonify(response), 500
