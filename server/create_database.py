426c9c7b69cd8291b036bcec009cb30e46d59fd6
amlannandy <amlannandy5@gmail.com>
2021-12-24 22:56:01 +0530

    remove flutter app

--- server/create_database.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('..')
-
-from server.app import app, db
-from server.models.Crop import Crop
-from server.models.Record import Record
-
-with app.app_context():
-    db.create_all()
